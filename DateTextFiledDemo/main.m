dcb1da08f05504422f4ce88e0e454d7c2b6da1a1
chlewsz <chlewsz@163.com>
2016-09-19 08:47:22 +0800

    Initial Commit

---
+++ DateTextFiledDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DateTextFiledDemo
+//
+//  Created by wsz on 16/9/19.
+//  Copyright © 2016年 heiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
